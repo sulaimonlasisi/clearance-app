395fa771d5f32002010aaa348423facc2621fe86
Sulai <seun.lasisi@yahoo.com>
2018-06-06 23:16:15 -0400

    Refactored and ready for review

--- src/ratings/RatingsClient.js
+++ src/ratings/RatingsClient.js
@@ -21,7 +21,7 @@ class RatingsClient {
     Function that aggregates calls to get all items' information
   */
   async getAllItemsRatingsAndReviews(pairedProductList){
-    return this.allProdsReturn(pairedProductList).then((allProds) => {
+    return this._allProdsReturn(pairedProductList).then((allProds) => {
       let allProducts = new PairedProductList();
       allProds.forEach((product) => {
         allProducts.addPairedProduct(product.amazonProd, product.walmartProd);
@@ -29,29 +29,17 @@ class RatingsClient {
       return allProducts;
     });
   }
-  
-  timeout(ms) {
+  /*
+    delay function
+  */
+  _timeout(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
-  allProdsReturn(pairedProductList){
-    return new Promise ((resolve, reject) => {
-      try {
-        resolve(this.batchedRatingsAndReviewsRequest(pairedProductList));
-      }
-      catch(err){
-        console.log(err);
-        reject(err);
-      } 
-    })
-             
-  }
-
-
   /*
-    Batches ratings and reviews request
+    Return all products' ratings and reviews
    */
-  batchedRatingsAndReviewsRequest(pairedProductList) {
+  _allProdsReturn(pairedProductList) {
     return new Promise(async (resolve, reject) => {
       let promises = [];
       let index=0;
@@ -59,8 +47,8 @@ class RatingsClient {
       try {
         do {
           if (pairedProductList.products[index]) {
-            promises.push(await this.getSingleItemRatingAndReview(browser, pairedProductList.products[index]));
-            this.timeout(index*this.delayTime);
+            promises.push(await this._getSingleItemRatingAndReview(browser, pairedProductList.products[index]));
+            this._timeout(index*this.delayTime);
           }          
           index+=1;
         } while (index < pairedProductList.products.length);
@@ -76,19 +64,19 @@ class RatingsClient {
   }
 
   /*
-    Function that single item ratings and reviews to be used for further analysis
+    Function that returns single item rating and number of reviews to be used for further analysis
   */
-  async getSingleItemRatingAndReview(browser, product){
+  async _getSingleItemRatingAndReview(browser, product){
     try {      
       let page = await browser.newPage();
       let item_link = `${this.base_url}${product.amazonProd.ASIN}`;
       await page.goto(item_link);
       //get rating value
       let rating = await page.evaluate(() => document.querySelector(".a-size-base.a-color-secondary").innerText);
-      rating = this.parseRating(rating);
+      rating = this._parseRating(rating);
       //get number of reviews value
       let numReviews = await page.evaluate(() => document.querySelector(".a-size-small.a-link-emphasis").innerText);
-      numReviews = this.parseNumReviews(numReviews);
+      numReviews = this._parseNumReviews(numReviews);
       product.amazonProd.ratingsAndReviews = {itemRating: rating, itemNumReviews: numReviews};
       await page.close();
       return product;
@@ -99,13 +87,13 @@ class RatingsClient {
   }
 
 
-  // Returns a string of the basic product information.
-  parseRating(ratingString) {
+  // Returns a float of the rating string.
+  _parseRating(ratingString) {
     return parseFloat(ratingString.split(' ')[0]); 
   }
 
-  // Returns a string of the basic product information.
-  parseNumReviews(reviewString) {
+  // Returns an integer of the number of reviews.
+  _parseNumReviews(reviewString) {
     return parseInt(reviewString.split(' ')[2]); 
   }
 }
